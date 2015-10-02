896c13c072d59c0b0a90e843b948d36672dac39d
padnezz <padnezz@gmail.com>
2015-10-02 13:19:33 +0200

    Fixade input till .bash_profile

--- install.sh
+++ install.sh
@@ -10,13 +10,13 @@ echo "Installing Go Programming Language..."
 brew install go
 echo "Specify the full path of the Go Workspace: "
 read go_path
-echo -e "export GOPATH=$go_path\n" >> $HOME/.bash_profile
 echo "Settings commands..."
-echo -e "hm_update(){echo Updating...; cd $go_path/src/github.com; sudo rm -rf padnezz;}\n" >> $HOME/.bash_profile
-echo -e "hm_flood(){cd $go_path/src/github.com/padnezz/kahoot-hack-mod; go run flood.go;}\n" >> $HOME/.bash_profile
-echo -e "hm_crash(){cd $go_path/src/github.com/padnezz/kahoot-hack-mod; go run crash.go;}\n" >> $HOME/.bash_profile
-echo -e "hm_regular(){cd $go_path/src/github.com/padnezz/kahoot-hack-mod; go run regular.go;}\n" >> $HOME/.bash_profile
-cd go_path
+echo -e "hm_update(){echo Updating...; cd $go_path/src/github.com; sudo rm -rf padnezz;}" >> $HOME/.bash_profile
+echo -e "hm_flood(){cd $go_path/src/github.com/padnezz/kahoot-hack-mod; go run flood.go;}" >> $HOME/.bash_profile
+echo -e "hm_crash(){cd $go_path/src/github.com/padnezz/kahoot-hack-mod; go run crash.go;}" >> $HOME/.bash_profile
+echo -e "hm_regular(){cd $go_path/src/github.com/padnezz/kahoot-hack-mod; go run regular.go;}" >> $HOME/.bash_profile
+echo -e "export GOPATH=$go_path" >> $HOME/.bash_profile
+cd $go_path
 echo "Installing the hack and necessary components..."
 go get github.com/padnezz/kahoot-hack-mod
 go get github.com/gorilla/websocket
