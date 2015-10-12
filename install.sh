97904468c4c8cfdbc38e4c39339f2b4c32fc6c04
padnezz <padnezz@gmail.com>
2015-10-12 08:30:29 +0200

    Fixed code for .bash_profile

--- install.sh
+++ install.sh
@@ -11,10 +11,23 @@ brew install go
 echo "Specify the full path of the Go Workspace: "
 read go_path
 echo "Settings commands..."
-echo -e "hm_update(){echo Updating...; cd $go_path/src/github.com; sudo rm -rf padnezz; go get github.com/padnezz/kahoot-hack-mod;}" >> $HOME/.bash_profile
-echo -e "hm_flood(){cd $go_path/src/github.com/padnezz/kahoot-hack-mod; go run flood.go;}" >> $HOME/.bash_profile
-echo -e "hm_crash(){cd $go_path/src/github.com/padnezz/kahoot-hack-mod; go run crash.go;}" >> $HOME/.bash_profile
-echo -e "hm_regular(){cd $go_path/src/github.com/padnezz/kahoot-hack-mod; go run regular.go;}" >> $HOME/.bash_profile
+echo -e "hm_update(){
+echo Updating...
+cd $go_path/src/github.com; sudo rm -rf padnezz
+go get github.com/padnezz/kahoot-hack-mod
+}" >> $HOME/.bash_profile
+echo -e "hm_flood(){
+cd $go_path/src/github.com/padnezz/kahoot-hack-mod
+go run flood.go
+}" >> $HOME/.bash_profile
+echo -e "hm_crash(){
+cd $go_path/src/github.com/padnezz/kahoot-hack-mod
+go run crash.go
+}" >> $HOME/.bash_profile
+echo -e "hm_regular(){
+cd $go_path/src/github.com/padnezz/kahoot-hack-mod
+go run regular.go
+}" >> $HOME/.bash_profile
 echo -e "export GOPATH=$go_path" >> $HOME/.bash_profile
 cd $go_path
 echo "Installing the hack and necessary components..."
