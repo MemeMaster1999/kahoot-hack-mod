329e8ab7f7bb5ffe52dbe1f338a622e6cb4bb66e
MemeMaster1999 <50968104+MemeMaster1999@users.noreply.github.com>
2019-05-23 17:58:00 -0400

    Update regular.go

--- regular.go
+++ regular.go
@@ -11,9 +11,9 @@ func main() {
 	var pin string
 	var nickname string
 	var answer string
-	fmt.Print("Game pin: ")
+	fmt.Print("Game pin:704116 ")
 	fmt.Scanln(&pin)
-	fmt.Print("Nickname: ")
+	fmt.Print("Nickname:Lil hac ")
 	fmt.Scanln(&nickname)
 	fmt.Println("Connecting...")
 	conn, err := kahoot.NewConnection(pin)
@@ -28,7 +28,7 @@ func main() {
 	for {
 		fmt.Print("Enter to answer question: ")
 		fmt.Scanln(&answer)
-		fmt.Print("Answer: ")
+		fmt.Print("Answer: 23")
 		var number string
 		fmt.Scanln(&number)
 		parsed, _ := strconv.Atoi(number)
